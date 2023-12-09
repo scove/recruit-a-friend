36c759d1e8e5395083a2d98bea94ee3d62a38d52
Sam <scove003@plattsburgh.edu>
2023-12-09 15:33:33 -0500

    skeleton for insertion queries - insecure rn

--- src/query_builder.py
+++ src/query_builder.py
@@ -11,22 +11,39 @@ class QueryBuilder():
     def build_query(self):
         pass
 
+    @abstractmethod
+    def interpret_list(self):
+        pass
+
 class AddFriendQuery(QueryBuilder):
     def __init__(self, input_data: RequiredData):
         super().__init__(input_data)
+        self.interpret_list()
     
+    def interpret_list(self):
+        temp = ""
+        for i in fields(self.data):
+            if i.name == 'interests':
+                interest_values = getattr(self.data, i.name)
+                temp = ('@@@'.join(interest_values))
+                setattr(self.data, i.name, temp)
+                break
+
     def build_query(self):
-        #"INSERT INTO friends () VALUES ();"
         columns = []
         values = []
+        sql_columns = ""
+        sql_values = ""
+        #all of this needs to be handled for sql injection
         for i in fields(self.data):
-            columns.append(i.name)
-            if i.name == 'interests':
-                processed = '| '.join(getattr(self.data, i.name))
-                values.append(processed)
+            #check if null
+            value_of_column = getattr(self.data, i.name)
+            if value_of_column == None:
                 continue
-            values.append(getattr(self.data, i.name))
-        columns = ', '.join(columns)
-        values = ', '.join(values) #TODO:failing on null
-        query_string = f"INSERT INTO FRIENDS ({columns}) VALUES ({values});" #this is insecure rn
-        print(query_string)
+            columns.append(i.name)
+            values.append(value_of_column)
+        
+        sql_columns = (', '.join(columns))
+        sql_values = (', '.join(values))
+        query_string = f"INSERT INTO FRIENDS ({sql_columns}) VALUES ({sql_values});" #this is insecure rn
+        return query_string
--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -4,11 +4,18 @@ from query_builder import AddFriendQuery
 
 
 #mock data
-jimmy = Friend("jimmy", "jimmy123@jimmymail.com", "123456", ['baseball', 'whatever'])
-jammy = Friend("jammy", "jammy@whatever.com", None, ["soccer"])
+jimmy = Friend("jimmy", "jimmy123@jimmymail.com", "1234567", ["baseball", "whatever"]) #do not build twice, will not be fun
+jammy = Friend("jammy", "jammy@whatever.com", None, ["soccer", "whatever"])
+tammy = Friend("tammy", None, None, ["sales"])
 
 class AddFriendQueryTester(TestCase):
     def test_should_create_query(self):
         assert isinstance(AddFriendQuery(jimmy), AddFriendQuery)
-        AddFriendQuery(jammy).build_query()
-        assert False
+    
+    def test_should_interpret_list(self):
+        expected_return_value = 'soccer@@@whatever'
+        assert AddFriendQuery(jammy).data.interests == expected_return_value
+    
+    def test_should_build_query(self):
+        expected_return_value = "INSERT INTO FRIENDS (name, interests) VALUES (tammy, sales);"
+        assert AddFriendQuery(tammy).build_query() == expected_return_value
